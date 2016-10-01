94f01834a78a08e3c687e069c721a40c47a1b942
Nhan Ho <human@nhanho.me>
2016-10-01 14:47:25 +0700

    Add CLI to import users

--- screeps_loan/screeps_loan.py
+++ screeps_loan/screeps_loan.py
@@ -7,6 +7,7 @@ from screeps_loan.auth_user import AuthPlayer
 import socket
 from screeps_loan.models.db import get_conn
 import screeps_loan.screeps_client as screeps_client
+import screeps_loan.cli.import_user
 
 
 @app.route('/')
