d06e4d16d92f677373e503113625a20148776cc8
Robert Hafner <tedivm@tedivm.com>
2016-10-08 00:15:07 -0700

    Add optional "import_id" to get_all_rooms

--- screeps_loan/models/rooms.py
+++ screeps_loan/models/rooms.py
@@ -1,10 +1,12 @@
 from screeps_loan.models import db
 
 
-def get_all_rooms():
-    query = "SELECT id FROM room_imports WHERE status LIKE 'complete' ORDER BY started_at DESC"
-    result = db.find_one(query)
-    import_id = result[0]
+def get_all_rooms(import_id=None):
+
+    if not import_id:
+        query = "SELECT id FROM room_imports WHERE status LIKE 'complete' ORDER BY started_at DESC"
+        result = db.find_one(query)
+        import_id = result[0]
 
     query = "SELECT name, level, ign FROM rooms JOIN users ON rooms.owner = users.id WHERE import=(%s)"
     result = db.find_all(query, (import_id, ))
