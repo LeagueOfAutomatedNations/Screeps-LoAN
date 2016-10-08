f37b61664b8b28b5f595ef52e4979d61df8ec599
Robert Hafner <tedivm@tedivm.com>
2016-10-08 00:07:05 -0700

    Restrict room check to most recent completed import

--- screeps_loan/models/rooms.py
+++ screeps_loan/models/rooms.py
@@ -1,7 +1,12 @@
 from screeps_loan.models import db
 
+
 def get_all_rooms():
-    query = "SELECT name, level, ign FROM rooms JOIN users ON rooms.owner = users.id"
-    result = db.find_all(query)
+    query = "SELECT id FROM room_imports WHERE status LIKE 'complete' ORDER BY started_at DESC"
+    result = db.find_one(query)
+    import_id = result[0]
+
+    query = "SELECT name, level, ign FROM rooms JOIN users ON rooms.owner = users.id WHERE import=(%s)"
+    result = db.find_all(query, (import_id, ))
     return [{'roomname': item[0], 'level': item[1], 'owner_name':item[2]} for item in result]
 
