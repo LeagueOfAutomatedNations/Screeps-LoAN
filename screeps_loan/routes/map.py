4912dc8d145385764f2cf06a64690f3b93991e1f
Robert Hafner <tedivm@tedivm.com>
2016-10-09 13:20:49 -0700

    Move room data to separate json file, consolidate map stuff to own route file

---
+++ screeps_loan/routes/map.py
@@ -0,0 +1,19 @@
+from screeps_loan import app
+from screeps_loan.models.rooms import get_all_rooms
+import json
+from flask import render_template
+
+
+@app.route('/rooms.js')
+def alliance_room_json():
+    room_data = get_all_rooms()
+    room_data_aux = {}
+    for room in room_data:
+        room_data_aux[room['roomname']] = {'level': room['level'], 'owner': room['owner_name']}
+
+    return json.dumps(room_data_aux)
+
+
+@app.route('/map')
+def map():
+    return render_template("map.html")
