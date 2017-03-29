4f5afb409049ebe55f0487d28f1e92d860b8aaca
Robert Hafner <tedivm@tedivm.com>
2017-03-29 01:16:01 -0700

    expose alliance ranking stats via json, including in alliances.js

--- screeps_loan/models/rankings.py
+++ screeps_loan/models/rankings.py
@@ -1,6 +1,11 @@
 from screeps_loan.models import db
 from screeps_loan.services.cache import cache
 
+
+def get_rankings_list():
+    return ['alliance_gcl', 'combined_gcl', 'rcl', 'spawns', 'members']
+
+
 def get_all_rankings(import_id=None):
     if not import_id:
         query = "SELECT id FROM rankings_imports WHERE status LIKE 'complete' ORDER BY started_at DESC"
@@ -9,7 +14,42 @@ def get_all_rankings(import_id=None):
     return get_all_rankings_by_import(import_id)
 
 
+def get_latest_import_id():
+    query = "SELECT id FROM rankings_imports WHERE status LIKE 'complete' ORDER BY started_at DESC"
+    result = db.find_one(query)
+    return result[0]
+
+
 @cache.cache()
 def get_all_rankings_by_import(import_id):
     query = "SELECT * FROM rankings WHERE import=(%s) ORDER BY alliance_gcl DESC"
     return db.find_all(query, (import_id, ))
+
+
+@cache.cache()
+def get_rankings_by_import_and_type(ranking_type, import_id=None):
+    allowed_types = get_rankings_list()
+    if ranking_type not in allowed_types:
+        raise ValueError('Not a valid ranking type')
+    if not import_id:
+        import_id = get_latest_import_id()
+
+    query = "SELECT alliance FROM rankings WHERE import=(%s) ORDER BY " + ranking_type + " DESC"
+    results = db.find_all(query, (import_id, ))
+    alliance_mapping = {}
+    for index in range(len(results)):
+        alliance_mapping[results[index][0]] = index+1
+    return alliance_mapping
+
+
+@cache.cache()
+def get_rankings_by_alliance_and_type(ranking_type, alliance, import_id=None):
+    allowed_types = get_rankings_list()
+    if ranking_type not in allowed_types:
+        raise ValueError('Not a valid ranking type')
+
+    if not import_id:
+        import_id = get_latest_import_id()
+
+    query = "SELECT " + ranking_type + " FROM rankings WHERE alliance=(%s) AND import=(%s) ORDER BY " + ranking_type + " DESC"
+    return db.find_all(query, (alliance, import_id))
