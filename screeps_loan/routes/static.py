bac1acdbd5e3f14b86e585ba3282a731d9aa3d06
Robert Hafner <tedivm@tedivm.com>
2017-12-03 17:06:34 -0800

    Add API documentation

--- screeps_loan/routes/static.py
+++ screeps_loan/routes/static.py
@@ -5,3 +5,8 @@ from screeps_loan import app
 @app.route('/tools')
 def static_tools():
     return render_template('tools.html')
+
+
+@app.route('/api')
+def static_api():
+    return render_template('api.html')
