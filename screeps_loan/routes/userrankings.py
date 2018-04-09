7acccb2daf5c7a4f560591cd82934d5da0c5b4e1
Robert Hafner <tedivm@tedivm.com>
2018-04-08 22:47:56 -0700

    Fix cases where gcl and power are not set in the database

--- screeps_loan/routes/userrankings.py
+++ screeps_loan/routes/userrankings.py
@@ -8,14 +8,14 @@ combined_min = 1000000
 
 @app.route('/player/rankings/gcl')
 def player_rankings_gcl():
-    userlist = [elem for elem in users.get_all_users() if elem['gcl'] > gcl_min]
+    userlist = [elem for elem in users.get_all_users() if isinstance(elem['gcl'], int) and elem['gcl'] > gcl_min]
     userlist.sort(key = lambda a: a['gcl'])
     return render_template("user_rankings.html", rankings=userlist[::-1], field='gcl', category='GCL')
 
 
 @app.route('/player/rankings/power')
 def player_rankings_power():
-    userlist = [elem for elem in users.get_all_users() if elem['gcl'] > power_min]
+    userlist = [elem for elem in users.get_all_users() if isinstance(elem['power'], int) and elem['power'] > power_min]
     userlist.sort(key = lambda a: a['power'])
     return render_template("user_rankings.html", rankings=userlist[::-1], field='power', category='Power')
 
@@ -25,11 +25,17 @@ def player_rankings_combined():
     userlist = users.get_all_users()
     finalusers = []
     for user in userlist:
-        combined = (user['power'] * 50) + user['gcl']
+        combined = 0
+        if 'power' in user and isinstance(user['power'], int):
+            combined = user['power'] * 50
+
+        if 'gcl' in user and isinstance(user['gcl'], int):
+            combined = user['gcl']
+
         if combined > combined_min:
             finalusers.append({
                 'ign': user['ign'],
-                'combined': (user['power'] * 50) + user['gcl']
+                'combined': combined
             })
     finalusers.sort(key = lambda a: a['combined'])
     return render_template("user_rankings.html", rankings=finalusers[::-1], field='combined', category='Combined')
