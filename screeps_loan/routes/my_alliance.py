8b5df5db471479ecfe8363a4862c82dd921b8f71
Randy <phpgeweld@gmail.com>
2019-04-20 11:38:23 +0200

    Removed the external API call to check if a user exists
    Feedback from qgazq

--- screeps_loan/routes/my_alliance.py
+++ screeps_loan/routes/my_alliance.py
@@ -165,16 +165,6 @@ def kick_from_alliance():
 
     username = request.form['username']
 
-    # Make sure user is in database and game world.
-    api = screeps_client.get_client()
-    auth = AuthPlayer(api)
-    ign = auth.id_from_name(username)
-
-    if not ign:
-        flash('User not present in game - remember usernames are case sensitive.')
-        return redirect(url_for("my_alliance"))
-
-
     # Get database id
     user_id = users_model.user_id_from_db(username)
 
