e54508f53f1895905ca5ceda0dc64def93bfb655
Randy <phpgeweld@gmail.com>
2019-04-24 20:56:42 +0200

    CLI methods for managing alliance leaders

--- screeps_loan/cli/manage.py
+++ screeps_loan/cli/manage.py
@@ -1,6 +1,7 @@
 import click
 from screeps_loan import app
 from screeps_loan.models import users
+import screeps_loan.models.alliances_leaders as alliances_leaders_model
 import screeps_loan.screeps_client as screeps_client
 from screeps_loan.auth_user import AuthPlayer
 
@@ -32,3 +33,28 @@ def login_as_user(user):
 @click.argument('alliance')
 def room_count(alliance):
     click.echo(alliances_model.get_room_count(alliance))
+
+
+@app.cli.command()
+@click.argument('alliance')
+@click.argument('user')
+def remove_alliance_leader(alliance, user):
+    user_id = users.user_id_from_db(user)
+    if user_id:
+        alliances_leaders_model.remove_leader(alliance, user_id)
+    else:
+        click.echo('user not found')
+
+@app.cli.command()
+@click.argument('alliance')
+@click.argument('user')
+def add_alliance_leader(alliance, user):
+    user_id = users.user_id_from_db(user)
+    if user_id:
+        alliance = alliances_model.find_by_shortname(alliance)
+        if alliance:
+            alliances_leaders_model.add_leader(alliance, user_id)
+        else:
+            click.echo('alliance not found')
+    else:
+        click.echo('user not found')
