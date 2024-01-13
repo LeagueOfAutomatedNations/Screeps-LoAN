23849c5127ac31b34d4a1d6c654137e102f541b7
pieterbrandsen <pieterbrandsen2@gmail.com>
2024-01-13 17:57:03 +0100

    Removed json from accept invite

--- screeps_loan/routes/invites.py
+++ screeps_loan/routes/invites.py
@@ -57,10 +57,6 @@ def invite_accept(inviteid):
         abort(404)
 
     # Add user to appropriate alliance.
-    import json 
-    json_mylist = json.dumps(invite)
-    flash(my_id)
-    flash(json_mylist)
     users_model.update_alliance_by_user_id(my_id, invite["alliance_id"])
 
     # Remove all pending invites from user.
