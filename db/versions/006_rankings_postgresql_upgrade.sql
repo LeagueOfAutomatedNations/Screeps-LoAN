6a7f5187e48ac93f10a3cd101d903c9409e9cb93
Etienne Samson <samson.etienne@gmail.com>
2024-01-13 13:12:42 +0100

    Make the latest DB changes into actual migrations

--- db/versions/006_rankings_postgresql_upgrade.sql
+++ db/versions/006_rankings_postgresql_upgrade.sql
@@ -1,7 +1,7 @@
 CREATE TABLE rankings_imports (
-  id integer NOT NULL,
-  started_at timestamp without time zone default current_timestamp,
-  status character varying(50) NOT NULL
+    id integer NOT NULL,
+    started_at timestamp without time zone default current_timestamp,
+    status character varying(50) NOT NULL
 );
 
 CREATE SEQUENCE rankings_imports_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE NO MAXVALUE CACHE 1;
@@ -9,29 +9,28 @@ CREATE SEQUENCE rankings_imports_id_seq START WITH 1 INCREMENT BY 1 NO MINVALUE 
 ALTER SEQUENCE rankings_imports_id_seq OWNED BY rankings_imports.id;
 
 ALTER TABLE
-  ONLY rankings_imports
+    ONLY rankings_imports
 ALTER COLUMN
-  id
+    id
 SET
-  DEFAULT nextval('rankings_imports_id_seq' :: regclass);
+    DEFAULT nextval('rankings_imports_id_seq' :: regclass);
 
 ALTER TABLE
-  ONLY rankings_imports
+    ONLY rankings_imports
 ADD
-  CONSTRAINT rankings_imports_pkey PRIMARY KEY (id);
+    CONSTRAINT rankings_imports_pkey PRIMARY KEY (id);
 
 CREATE TABLE rankings (
-  alliance character varying(255) NOT NULL,
-  import integer NOT NULL,
-  alliance_gcl integer NOT NULL,
-  combined_gcl integer NOT NULL,
-  average_gcl integer NOT NULL,
-  rcl integer NOT NULL,
-  spawns integer NOT NULL,
-  members integer NOT NULL
+    alliance character varying(255) NOT NULL,
+    import integer NOT NULL,
+    alliance_gcl integer NOT NULL,
+    combined_gcl integer NOT NULL,
+    rcl integer NOT NULL,
+    spawns integer NOT NULL,
+    members integer NOT NULL
 );
 
 ALTER TABLE
-  ONLY rankings
+    ONLY rankings
 ADD
-  CONSTRAINT rankings_pkey PRIMARY KEY (alliance, import);
+    CONSTRAINT rankings_pkey PRIMARY KEY (alliance, import);
---
+++ db/versions/013_more_alliance_stats_postgresql_downgrade.sql
@@ -0,0 +1,7 @@
+ALTER TABLE
+    rankings DROP COLUMN average_gcl;
+
+ALTER TABLE
+    users DROP COLUMN gcl_level,
+    DROP COLUMN combined_rcl,
+    DROP COLUMN spawnCount;
